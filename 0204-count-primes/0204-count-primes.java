6e478dda3147a3b04d3b021d8a57bb2cc2db9f21
shivamsingh831703-hash <shivamsingh831703@gmail.com>
2026-08-07 22:27:55 +0530

    Time: 100 ms (65.69%), Space: 51.4 MB (22.85%) - LeetHub

---
+++ 0204-count-primes/0204-count-primes.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int countPrimes(int n) {
+     boolean[] notPrime = new boolean[n];
+     int count = 0;
+    for(int i = 2; i < n; i++){
+        if(!notPrime[i]){
+            count++;
+            for(int j = i + i; j < n; j += i){
+              notPrime[j] = true;
+            }
+        }
+    }
+    return count;
+    
+    }
+}
