caa87747d263d803aa78a3fd6a7c9d8220dedf1f
shivamsingh831703-hash <shivamsingh831703@gmail.com>
2026-08-16 21:13:11 +0530

    Time: 1 ms (99.87%), Space: 47.2 MB (20.29%) - LeetHub

---
+++ 1903-largest-odd-number-in-string/1903-largest-odd-number-in-string.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String largestOddNumber(String num) {
+       if((int)num.charAt(num.length()-1)%2!=0) return num;
+        int i=num.length()-1;
+        while(i>=0){
+            if(num.charAt(i)%2!=0){
+                return num.substring(0,i+1);
+            }
+            i--;
+        }
+        return "";
+}
+}
