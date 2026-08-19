4b60bf5314c2917c750d1a52d0c256210b429a24
shivamsingh831703-hash <shivamsingh831703@gmail.com>
2026-08-19 20:04:59 +0530

    Time: 5 ms (5.4%), Space: 43.9 MB (13.59%) - LeetHub

---
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.java
@@ -0,0 +1,21 @@
+class Solution {
+    public String mergeAlternately(String word1, String word2) {
+          String ans = "";
+        int i = 0;
+
+        while (i < word1.length() || i < word2.length()) {
+
+            if (i < word1.length()) {
+                ans = ans + word1.charAt(i);
+            }
+
+            if (i < word2.length()) {
+                ans = ans + word2.charAt(i);
+            }
+
+            i++;
+        }
+
+        return ans;
+    }
+}
