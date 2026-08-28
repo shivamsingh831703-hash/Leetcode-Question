64f98cc24eb887f456fffbf922fb2cc26b8c9a6d
shivamsingh831703-hash <shivamsingh831703@gmail.com>
2026-08-28 21:19:37 +0530

    Time: 3 ms (41.96%), Space: 43.2 MB (37.02%) - LeetHub

---
+++ 0389-find-the-difference/0389-find-the-difference.java
@@ -0,0 +1,17 @@
+class Solution {
+    public char findTheDifference(String s, String t){
+  int sumS = 0;
+        int sumT = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            sumS += s.charAt(i);
+        }
+
+        for (int i = 0; i < t.length(); i++) {
+            sumT += t.charAt(i);
+        }
+
+        return (char)(sumT - sumS);
+    } 
+        
+    }
