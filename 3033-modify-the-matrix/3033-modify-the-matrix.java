ee20bac1740b8ec35a87533ab8a184ce889a0997
shivamsingh831703-hash <shivamsingh831703@gmail.com>
2026-09-03 23:42:13 +0530

    Time: 1 ms (99.86%), Space: 47.3 MB (49.08%) - LeetHub

---
+++ 3033-modify-the-matrix/3033-modify-the-matrix.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int[][] modifiedMatrix(int[][] matrix) {
+         int m = matrix.length;
+        int n = matrix[0].length;
+        for (int j = 0; j < n; j++) {
+            int max = 0;
+
+            for (int i = 0; i < m; i++) {
+                max = Math.max(max, matrix[i][j]);
+            }
+            for (int i = 0; i < m; i++) {
+                if (matrix[i][j] == -1) {
+                    matrix[i][j] = max;
+                }
+            }
+        }
+
+        return matrix;
+    }
+}
