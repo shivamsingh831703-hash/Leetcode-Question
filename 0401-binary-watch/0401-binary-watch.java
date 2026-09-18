89e2a5aee5afa462d02899c2e3d677c2187d055a
shivamsingh831703-hash <shivamsingh831703@gmail.com>
2026-09-18 17:17:31 +0530

    Time: 14 ms (13.28%), Space: 49.9 MB (7.25%) - LeetHub

---
+++ 0401-binary-watch/0401-binary-watch.java
@@ -0,0 +1,20 @@
+class Solution {
+    public List<String> readBinaryWatch(int turnedOn) {
+          List<String> ans = new ArrayList<>();
+
+        for (int hour = 0; hour < 12; hour++) {
+
+            for (int minute = 0; minute < 60; minute++) {
+
+                int count = Integer.bitCount(hour) 
+                          + Integer.bitCount(minute);
+
+                if (count == turnedOn) {
+                    ans.add(hour + ":" + String.format("%02d", minute));
+                }
+            }
+        }
+
+        return ans;
+    }
+}
